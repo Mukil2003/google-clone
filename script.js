31f9ac43597903f463c292e148a5695ca3373418
Mukil2003 <sparkle15803@gmail.com>
2024-02-13 22:08:24 +0530

    Task completed

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function search_url() {
   let url = "https://www.google.com/search?q=" + searchUrl;
   window.open(url, "google");
 }
+
+document
+  .getElementById("input_value")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      search_url();
+    }
+  });
